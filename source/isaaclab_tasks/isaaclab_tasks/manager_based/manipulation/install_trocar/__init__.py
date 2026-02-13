192ba50963106eaa80f010468f3aac1075ae945c
Yun Liu <yunl@nvidia.com>
2026-02-13 19:18:17 +0800

    remove unused file
    Signed-off-by: Yun Liu <yunl@nvidia.com>

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/install_trocar/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2022-2026, The Isaac Lab Project Developers.
-# All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
-
-"""Configurations for the Install Trocar task with G129 + Dex3 robot."""
-
-from .config import *  # noqa: F401, F403
--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/install_trocar/config/g129_dex3/agents/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2022-2026, The Isaac Lab Project Developers.
-# All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
-
-"""Agent configurations for Install Trocar task with G129 + Dex3."""
-
-from .rlinf_ppo_cfg import RLinfInstallTrocarPPOCfg
